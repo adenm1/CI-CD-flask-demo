96e69d0cf626eac09448b60cc7f7937692650e97
Liu Siying <2648619584@qq.com>
2025-11-16 14:36:37 +0800

    fix: remove duplicate layout file and enable prerendering

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,7 +1,8 @@
 import type { LayoutLoad } from './$types';
 import { env } from '$env/dynamic/public';
 
-export const prerender = false;
+export const prerender = true;
+export const ssr = false;
 
 export const load: LayoutLoad = async () => {
   const apiBaseUrl = env.PUBLIC_API_BASE_URL || 'http://localhost:8000';
